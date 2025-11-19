7f1997ffa8ca09aff37057d378a6469f0242a17a
vanhog <dieter@hoogestraat.dk>
2025-11-19 15:17:00 +0100

    Added age calculation

--- forms_and_values/script.js
+++ forms_and_values/script.js
@@ -11,3 +11,17 @@ function double() {
     document.getElementById('formInput').value = '';
   }
 }
+
+function age() {
+  yob = document.getElementById('formInput').value;
+
+  if (!isNaN(yob)) {
+    thisDate = new Date(Date.now());
+    thisAge = thisDate.getFullYear() - yob;
+    document.getElementById('result').innerHTML = thisAge;
+    document.getElementById('actionMessage').innerHTML = 'Your age in years is';
+  } else {
+    document.getElementById('actionMessage').innerHTML =
+      'Please enter your year of birth.';
+  }
+}
