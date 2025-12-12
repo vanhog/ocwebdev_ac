7df2bcaf7762c1745307ad92d02c4e6609356750
vanhog <dieter@hoogestraat.dk>
2025-12-12 09:59:12 +0100

    VAT app: del error msg on calculate

--- vatapp/scr/script.js
+++ vatapp/scr/script.js
@@ -1,3 +1,4 @@
+// variables.......................................................................
 const mode_remove = document.getElementById('removevat');
 const mode_add = document.getElementById('addvat');
 
@@ -10,14 +11,7 @@ const per19 = document.getElementById('per19');
 let vatRate = 19;
 let mode = 'add';
 
-console.log(document.getElementById('givenAmount'));
-
-function toggleLabels() {
-  amountlabelnet.classList.toggle('hidden');
-  amountlabelgross.classList.toggle('hidden');
-  resultlabelnet.classList.toggle('hidden');
-  resultlabelgross.classList.toggle('hidden');
-}
+// event listeners..................................................................
 mode_remove.addEventListener('change', function (event) {
   mode = 'remove';
   toggleLabels();
@@ -44,15 +38,27 @@ per19.addEventListener('change', function (event) {
   setVAT();
 });
 
+// functions.........................................................................
+function toggleLabels() {
+  amountlabelnet.classList.toggle('hidden');
+  amountlabelgross.classList.toggle('hidden');
+  resultlabelnet.classList.toggle('hidden');
+  resultlabelgross.classList.toggle('hidden');
+}
+
 function setVAT() {
   let givenAmount = document.getElementById('amount').value;
-  // console.log(givenAmount);
   if (isNaN(givenAmount)) {
     document.getElementById('amount').value = '0.00';
     document.getElementById('amount_error').classList.remove('hidden');
-    //window.alert('Please enter a valid amount.');
   } else {
     let vatfactor = (100 + Number(vatRate)) / 100;
+
+    // check if there's an error message pending even though there's no
+    // error anymore
+    if (!document.getElementById('amount_error').classList.contains('hidden')) {
+      clear_errorMsg();
+    }
 
     if (mode === 'add') {
       resAmount = (Number(givenAmount) * vatfactor).toFixed(2);
@@ -68,7 +74,6 @@ function setVAT() {
     ).innerHTML = `${resAmount} &#8364;`;
     document.getElementById('amount').value = Number(givenAmount).toFixed(2);
   }
-  //console.log(resAmount, resVAT);
 }
 
 function clear_errorMsg() {
