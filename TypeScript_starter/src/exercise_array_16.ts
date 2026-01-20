29ce42d6fc3e7e88354103b58e4104d7d11599c3
vanhog <dieter@hoogestraat.dk>
2026-01-20 17:21:51 +0100

    Exercixex 16 and 17

---
+++ TypeScript_starter/src/exercise_array_16.ts
@@ -0,0 +1,4 @@
+const numArray1: number[] = [36, 24, 22, 3, 2, 98, 88, 99, 10, 54, 68, 70];
+
+console.log(numArray1.sort((a, b) => a - b));
+console.log(numArray1.sort((a, b) => b - a));
---
+++ TypeScript_starter/src/exercise_array_17.ts
@@ -0,0 +1,2 @@
+const numArray: number[] = [36, 324, 122, 62, 98, 88, 99, 1000];
+console.log(numArray.sort((a, b) => a - b));
